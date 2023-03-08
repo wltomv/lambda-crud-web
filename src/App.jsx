8fb1b40b2afeb0380c96aa5fb7eeb2389250d4b6
Walter Mach <owaldovelasz@gmail.com>
2023-03-07 21:41:28 -0600

    Add button to add a new user

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import Grid from "./containers/Grid/Grid";
+import { BsPersonFillAdd } from "react-icons/bs";
 
 function App() {
 	return (
 		<div className="App">
+			<div className="add">
+				<button type="button">
+					<BsPersonFillAdd /> Agregar
+				</button>
+			</div>
 			<Grid />
 		</div>
 	);
